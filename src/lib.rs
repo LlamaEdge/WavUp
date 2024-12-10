c5ed3331f3609c40d9c4763ab8d8a89c738e43e8
Xin Liu <sam@secondstate.io>
2024-12-10 13:48:24 +0800

    refactor: improve `trim_ending_silence` func
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -135,7 +135,10 @@ impl AudioConverter {
         let original_sample_rate = track_info.sample_rate.unwrap();
 
         #[cfg(feature = "logging")]
-        debug!(target: "stdout", "track_info: {:?}", track_info);
+        {
+            debug!(target: "stdout", "channels: {}", channels);
+            debug!(target: "stdout", "original_sample_rate: {}", original_sample_rate);
+        }
 
         // Set up WAV writer
         let spec = WavSpec {
@@ -144,6 +147,11 @@ impl AudioConverter {
             bits_per_sample: 16,
             sample_format: hound::SampleFormat::Int,
         };
+
+        #[cfg(feature = "logging")]
+        info!(target: "stdout", "generated wav spec: {:?}", spec);
+
+        // Create WAV writer
         let mut wav_writer = WavWriter::create(&self.output_path, spec)?;
 
         if original_sample_rate == self.target_sample_rate {
@@ -156,7 +164,7 @@ impl AudioConverter {
             )?;
 
             #[cfg(feature = "logging")]
-            info!(target: "stdout", "Writing audio to WAV file: {}", &self.output_path);
+            info!(target: "stdout", "Writing {} audio samples to WAV file: {}", all_samples.len(), &self.output_path);
 
             for sample in all_samples {
                 wav_writer.write_sample((sample * 32768.0_f32) as i16)?;
@@ -245,6 +253,9 @@ impl AudioConverter {
             }
         }
 
+        #[cfg(feature = "logging")]
+        info!(target: "stdout", "Finalizing WAV file");
+
         wav_writer.finalize()?;
 
         Ok(())
@@ -284,12 +295,15 @@ impl AudioConverter {
             all_samples.extend(sample_buf.samples().iter().copied());
         }
 
-        all_samples = self.trim_ending_silence(&all_samples, channels, original_sample_rate);
-
-        Ok(all_samples)
+        self.trim_ending_silence(&all_samples, channels, original_sample_rate)
     }
 
-    fn trim_ending_silence(&self, samples: &[f32], channels: usize, sample_rate: u32) -> Vec<f32> {
+    fn trim_ending_silence(
+        &self,
+        samples: &[f32],
+        channels: usize,
+        sample_rate: u32,
+    ) -> Result<Vec<f32>, Box<dyn std::error::Error>> {
         #[cfg(feature = "logging")]
         info!(target: "stdout", "Trimming ending silence");
         // -20 dB ≈ 0.1
@@ -311,8 +325,21 @@ impl AudioConverter {
             sample_rate
         );
 
+        if samples.len() % channels != 0 {
+            let err_msg = format!(
+                "The number of samples is not divisible by the number of channels. samples.len(): {}, channels: {}",
+                samples.len(),
+                channels
+            );
+
+            error!(target: "stdout", "{}", err_msg);
+
+            return Err(AudioConversionError::InvalidSampleCount(err_msg).into());
+        }
+
         // First pass: find the last non-silent sample
-        for i in (0..samples.len() - channels).rev().step_by(channels) {
+        let num_samples = samples.len() / channels;
+        for i in (0..num_samples).rev().step_by(channels) {
             let mut silent = true;
             for ch in 0..channels {
                 if !self.is_silent(samples[i + ch], threshold) {
@@ -329,9 +356,9 @@ impl AudioConverter {
         // Add a small buffer (e.g., 0.5 seconds) after the last non-silent sample
         let buffer_duration_secs = 0.5;
         let buffer_samples = (buffer_duration_secs * sample_rate as f32) as usize * channels;
-        let trim_index = (last_non_silent_index + buffer_samples).min(samples.len());
+        let trim_index = (last_non_silent_index + channels - 1 + buffer_samples).min(samples.len());
 
-        samples[..trim_index].to_vec()
+        Ok(samples[..trim_index].to_vec())
     }
 
     fn is_silent(&self, sample: f32, threshold: f32) -> bool {
